3499e03b0e50ab654dac131f250e1d0871a6a64d
CHENG Xin <mrchengxin@gmail.com>
2024-06-11 23:18:05 +0800

    make Dockerfile compatible with arm64

--- ai-model-workspace/install-Miniconda.sh
+++ ai-model-workspace/install-Miniconda.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p ${HOME}/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ${HOME}/miniconda3/miniconda.sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-$(arch).sh -O ${HOME}/miniconda3/miniconda.sh
 bash ${HOME}/miniconda3/miniconda.sh -b -u -p ${HOME}/miniconda3
 rm -rf ${HOME}/miniconda3/miniconda.sh
 ${HOME}/miniconda3/bin/conda init zsh
--- ai-model-workspace/install-trojan-go.sh
+++ ai-model-workspace/install-trojan-go.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 ZIP_NAME=trojan-go-linux-amd64
+if [ "$(arch)" == "aarch64" ]; then
+    ZIP_NAME=trojan-go-linux-arm
+fi
 
 # Download the zip file
 wget -O /tmp/${ZIP_NAME}.zip https://github.com/p4gefau1t/trojan-go/releases/download/v0.10.6/${ZIP_NAME}.zip
 
 # Unzip the file to a specific directory
-sudo unzip /tmp/${ZIP_NAME}.zip -d /usr/local/${ZIP_NAME}/
+sudo unzip /tmp/${ZIP_NAME}.zip -d /usr/local/trojan-go/
 rm /tmp/${ZIP_NAME}.zip

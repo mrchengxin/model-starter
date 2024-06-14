b9a027cc00ed28b7b4fa125537b638e8f9cbc0f4
CHENG Xin <cheng_xi@worksap.co.jp>
2024-06-14 19:49:03 +0800

    install Python packages for ComfyUI

--- comfyui-ready/install-ComfyUI.sh
+++ comfyui-ready/install-ComfyUI.sh
@@ -7,14 +7,14 @@ conda create -n comfyui python==3.11.9 -y
 echo -e "conda activate comfyui" >> ~/.zshrc
 echo -e "conda activate comfyui" >> ~/.bashrc
 
-# # install necessary libraries
-# pip install --no-cache-dir torch torchvision
-# # # to fix the no libGL.so.1 issue
-# pip install --no-cache-dir opencv-python-headless==4.8.1.78
-# pip install --no-cache-dir xformers --no-deps
+# install necessary libraries
+conda run --no-capture-output -n comfyui pip install --no-cache-dir torch torchvision
+# # to fix the no libGL.so.1 issue
+conda run --no-capture-output -n comfyui pip install --no-cache-dir opencv-python-headless==4.8.1.78
+conda run --no-capture-output -n comfyui pip install --no-cache-dir xformers --no-deps
 
-# # clone ComfyUI
-# cd ${INSTALL_DIR} || { echo "Failed to open ${INSTALL_DIR} directory"; exit 1; }
-# git clone https://github.com/comfyanonymous/ComfyUI.git
-# cd ${INSTALL_DIR}/ComfyUI || { echo "Failed to open ComfyUI directory"; exit 1; }
-# pip install --no-cache-dir -r requirements.txt
+# clone ComfyUI
+cd ${INSTALL_DIR} || { echo "Failed to open ${INSTALL_DIR} directory"; exit 1; }
+git clone https://github.com/comfyanonymous/ComfyUI.git
+cd ${INSTALL_DIR}/ComfyUI || { echo "Failed to open ComfyUI directory"; exit 1; }
+conda run --no-capture-output -n comfyui pip install --no-cache-dir -r requirements.txt

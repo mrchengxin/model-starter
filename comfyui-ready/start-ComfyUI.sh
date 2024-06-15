43988da6c628868a5d237464f445a2fb37f2da52
CHENG Xin <mrchengxin@gmail.com>
2024-06-15 15:05:10 +0800

    improve script message

--- comfyui-ready/start-ComfyUI.sh
+++ comfyui-ready/start-ComfyUI.sh
@@ -2,7 +2,7 @@
 
 # Install Python libraries when starting ComfyUI for the first time
 if [ ! -f "/opt/ComfyUI/INSTALLED" ]; then
-    echo -e "\033[0;32mWhen launching ComfyUI for the first time, install the necessary Python libraries...\033[0m"
+    echo -e "\033[0;32mFirst time launching ComfyUI, install the necessary Python libraries now...\033[0m"
     source /opt/ComfyUI/install-ComfyUI.sh
 fi
 
